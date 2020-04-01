3086d547c18c235e27fa5ce2403b57eb5ce36c33
Alex Mireles <alexander.j.mireles@gmail.com>
2020-04-01 00:10:32 -0400

    disable hardware accel for OBS capture

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
 export let mainWindow: BrowserWindow;
 
+app.disableHardwareAcceleration();
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
